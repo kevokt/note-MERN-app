dc20cfc12050995656bc666c4974f5b17fe9441f
kevokt <kevinoktavian08@gmail.com>
2024-10-23 12:26:35 +0700

    finsihed POST API

--- backend/controller/note.controller.js
+++ backend/controller/note.controller.js
@@ -1,12 +1,32 @@
 import mongoose from "mongoose";
 import Note from "../models/note.model.js"
 
+// READ NOTES
 export const getNotes = async (req, res) => {
     try {
         const notes = await Note.find({});
         res.status(200).json({ success: true, data: notes })
     } catch (error) {
-        console.error("Error in Get Notes", error.message);
+        console.error("Error in Get Notes: ", error.message);
         res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
+
+// CREATE NOTE
+export const createNotes = async (req, res) => {
+    const note = req.body;
+
+    if (!note.title || !note.content) {
+        return res.status(400).json({ success: false, message: "Please insert the title or content" });
+    }
+
+    // const newProduct = new Product(product);
+    const newNote = new Note(note);
+
+    try {
+        await newNote.save();
+        res.status(201).json({ success: true, message: "Note Successfully Created", data: newNote })
+    } catch (error) {
+        console.error("Error in Creating Note: ", error.message);
     }
 }
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 // import { createProduct, deleteProduct, getProducts, updateProduct } from "../controller/product.controller.js";
-import { getNotes } from "../controller/note.controller.js";
+import { getNotes, createNotes } from "../controller/note.controller.js";
 
 const router = express.Router();
 
 router.get("/", getNotes);
+
+router.post("/", createNotes)
 
 export default router;
